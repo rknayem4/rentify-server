e954affa6f1fe341561a2ff0764db5d283473ebb
rknayem4 <rknayem10@gmail.com>
2026-05-18 20:51:36 +0600

    first commite

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('rentify-cars')
+    const carCollection = db.collection('car-collection')
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Developers");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
